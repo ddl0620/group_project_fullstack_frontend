b3a4cd477dc04a76d7300ff7e4432e757496177b
Quoc Khanh Khong <khanhquoc1125@gmail.com>
2025-04-30 01:43:14 +0700

    ...

---
+++ src/pages/Notification/components/NotificationList.jsx
@@ -0,0 +1,17 @@
+import { NotificationItem } from '@/pages/Notification/components/NotificationItem.jsx';
+
+export function NotificationList({ notifications, onDelete, onMarkAsRead, onNotificationClick }) {
+  return (
+    <div className="divide-y">
+      {notifications.map((notification) => (
+        <NotificationItem
+          key={notification._id}
+          notification={notification}
+          onDelete={onDelete}
+          onMarkAsRead={onMarkAsRead}
+          onClick={onNotificationClick}
+        />
+      ))}
+    </div>
+  )
+}
